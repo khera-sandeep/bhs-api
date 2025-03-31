299978b35950ef32490779b16c648deb6ee47945
khera-sandeep <sandeepkhera18@gmail.com>
2025-03-31 15:52:01 +0530

    reverting changes for audition

--- src/routers/userregistration.js
+++ src/routers/userregistration.js
@@ -156,8 +156,8 @@ router.get('/userRegistration', auth, authorizationMiddleware(RoleEnum.ADMIN),
           }
           else {filter[queryKey] = value;}
         }
-        filter['preferredAuditionLocation']='Talwara';
-        filter['status']='registered';
+        // filter['preferredAuditionLocation']='Talwara';
+        // filter['status']='registered';
 
         let registrationList = await UserRegistration.find(filter)
             .select({
